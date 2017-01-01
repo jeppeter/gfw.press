4784fb88b1e6508638443ecbaa96acaac0d91418
jeppeter <jeppeter@gmail.com>
2017-01-01 14:18:38 +0800

    to make client.sh change

--- client.sh
+++ client.sh
@@ -3,4 +3,4 @@ cd `dirname $0`
 _java="java -Dfile.encoding=utf-8 -Dsun.jnu.encoding=utf-8 -Duser.timezone=Asia/Shanghai -Xms256M -Xmx256M -classpath `find ./lib/*.jar | xargs echo | sed 's/ /:/g'`:./bin"
 
 _pack="press.gfw"
-$_java $_pack.Windows
+$_java $_pack.Windows -c
